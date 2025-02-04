f06d92f8d7f958e3d938fea621c48338bba02527
alphappy <alphappyalphappy@gmail.com>
2025-02-04 12:46:10 -0600

    expand condition documentation

--- worlds/rain_world/conditions/classes.py
+++ worlds/rain_world/conditions/classes.py
@@ -13,17 +13,37 @@ class Condition:
 
 
 class Simple(Condition):
-    """Represents a simple check against a CollectionState."""
     def __init__(self, items: list[str] | str, count: Optional[int] = None,
                  unique: Optional[bool] = None, negative: bool = False,
                  locations: bool = False):
         """
+        Represents a simple check against a CollectionState.  There are several ways the arguments may be specified.
+
+        Check for at least 1 `"A"`:
+         - `Simple("A")`
+         - `Simple("A", 1)`
+
+         Check for at least 5 `"A"`:
+         - `Simple("A", 5)`
+
+         Check for `"A"`, `"B"`, and `"C"`:
+         - `Simple(["A", "B", "C"])`
+         - `Simple(["A", "B", "C"], 3)`
+
+         Check for any 2 of `"A"`, `"B"`, and `"C"`, ignoring duplicates (so `["A", "A"]` would not pass):
+         - `Simple(["A", "B", "C"], 2)`
+
+         Check for any 2 of `"A"`, `"B"`, and `"C"`, allowing duplicates (so `["A", "A"]` would pass):
+         - `Simple(["A", "B", "C"], 2, unique=False)`
+
         :param items:  The item or list of items to check against.
         :param count:  The number of items that must be found in the CollectionState.
         If unspecified, all items must be found (`count == len(items)` if `items` is a list, or 1 otherwise).
         :param unique:  Whether duplicate items should be ignored.
         If unspecified, `unqiue` is `False` if `items` is a string and `True` if it's a list.
         :param negative:  Whether the condition should be negated.
+        :param locations:  Whether to treat `items` as a list of locations to check for access to
+        rather than as a list of collected items.
         """
         self.unique: bool = unique or (type(items) == list)
         self.items: list[str] = [items] if type(items) == str else items
@@ -66,15 +86,18 @@ class Compound(Condition):
 
 
 class AnyOf(Compound):
+    """Represents multiple checks against a CollectionState, at least one of which must be satsified."""
     def __init__(self, *conditions: Condition):
         super().__init__(1, *conditions)
 
 
 class AllOf(Compound):
+    """Represents multiple checks against a CollectionState, all of which must be satsified."""
     def __init__(self, *conditions: Condition):
         super().__init__(len(conditions), *conditions)
 
 
+# The default "condition" which is always satisfied.
 ConditionBlank = Condition()
 
 
