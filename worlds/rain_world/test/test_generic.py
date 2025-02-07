819210541e9b984a2700cf61a6523928d6f56738
alphappy <alphappyalphappy@gmail.com>
2025-02-07 01:48:07 -0600

    add basic test classes for each gamestate except inv for now

--- worlds/rain_world/test/test_generic.py
+++ worlds/rain_world/test/test_generic.py
@@ -1,13 +1,45 @@
 from . import RainWorldTestBase
 
 
+class TestMonkVanilla(RainWorldTestBase):
+    options = {"which_gamestate": "monk_vanilla"}
+
+
 class TestSurvivorVanilla(RainWorldTestBase):
     options = {"which_gamestate": "survivor_vanilla"}
+
+
+class TestHunterVanilla(RainWorldTestBase):
+    options = {"which_gamestate": "hunter_vanilla"}
+
+
+class TestMonkMSC(RainWorldTestBase):
+    options = {"which_gamestate": "monk_msd"}
 
 
 class TestSurvivorMSC(RainWorldTestBase):
     options = {"which_gamestate": "survivor_msc"}
 
 
+class TestHunterMSC(RainWorldTestBase):
+    options = {"which_gamestate": "hunter_msc"}
+
+
+class TestGourmand(RainWorldTestBase):
+    options = {"which_gamestate": "gourmand"}
+
+
+class TestArtificer(RainWorldTestBase):
+    options = {"which_gamestate": "artificer"}
+
+
+class TestRivulet(RainWorldTestBase):
+    options = {"which_gamestate": "rivulet"}
+
+
 class TestSpearMSC(RainWorldTestBase):
     options = {"which_gamestate": "spearmaster"}
+
+
+class TestSaint(RainWorldTestBase):
+    options = {"which_gamestate": "saint"}
