90f40933242d6a1c364114c2d31d5bf08cf536c6
alphappy <alphappyalphappy@gmail.com>
2025-02-27 17:07:27 -0600

    add hardcoded static data exceptions

---
+++ worlds/rain_world/game_data/hardcode.py
@@ -0,0 +1,12 @@
+def apply_hardcoded_exceptions(data: dict):
+    for dlcstate, dlcstate_data in data.items():
+        for region, region_data in dlcstate_data.items():
+            for room, room_data in region_data.items():
+                for objtype, obj_data in room_data.get("objects", {}).items():
+
+                    # Karma flowers do not spawn for hunter.
+                    if objtype == "KarmaFlower":
+                        obj_data.setdefault("filter", {"Red"}).update({"Red"})
+
+    # This token is not reasonably accessible for Artificer or at all for Sofanthiel.
+    data["MSC"]["GW"]["GW_C09"]["shinies"]["BrotherLongLegs"]["filter"].update({"Artificer", "Inv"})
